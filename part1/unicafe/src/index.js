d94405ce24ef8a16f269c21d9b95ff2cd4c2e4b8
Amara Bamba <amara.dianney@gmail.com>
2021-01-13 10:43:25 +0000

    Part1 ~ 1.7: unicafe, step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -24,6 +24,9 @@ const App = () => {
                 <p>good {good}</p>
                 <p>neutral {neutral}</p>
                 <p>neutral {bad}</p>
+                <p>all {good+neutral+bad}</p>
+                <p>average {(good-bad)/(good+neutral+bad)}</p>
+                <p>positive {100*(good/(good+neutral+bad))} %</p>
             </div>
         </div>
     );
