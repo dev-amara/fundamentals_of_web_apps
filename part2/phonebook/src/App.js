bcd23d1d9b75135c9db40e60129cfff90dd60d94
Amara Bamba <amara.dianney@gmail.com>
2021-01-15 15:29:31 +0000

    Part2 ~ 2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' },
+    ]);
     const [nameAndNumber, setNameAndNumber] = useState({ name: '', number: '' });
+    const [newSearch, setNewSearch] = useState('');
 
     const { name, number } = nameAndNumber;
 
@@ -26,9 +32,25 @@ const App = () => {
         setNameAndNumber({ ...nameAndNumber });
     };
 
+    const handleSearch = (event) => setNewSearch(event.target.value);
+
+    const showPersons = () => {
+        const data = persons.filter((person) => person.name.toLowerCase().includes(newSearch.toLowerCase()));
+        return data.map((value) => (
+            <div key={value.name}>
+                {value.name} {value.number}
+            </div>
+        ));
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                <p>filter shown with</p>
+                <input onChange={handleSearch} value={newSearch} name="search" />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={handleSubmit}>
                 <div>
                     name: <input onChange={handleChange} value={name} name="name" />
@@ -41,13 +63,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            <div>
-                {persons.map((value) => (
-                    <div key={value.name}>
-                        {value.name} {value.number}
-                    </div>
-                ))}
-            </div>
+            <div>{showPersons()}</div>
         </div>
     );
 };
