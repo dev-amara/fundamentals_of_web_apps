5a78cd8eeae0793ec4e5726b38ccbd34a30fefaa
Amara Bamba <amara.dianney@gmail.com>
2021-01-18 14:34:11 +0000

    Part3 ~ 3.3: Phonebook backend step3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,16 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = persons.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
 app.get("/info", (request, response) => {
   const count = persons.length;
   const pagePhoneBookInfo = `<div><p>Phonebook has info for ${count} people</p><p>${new Date()}</p></div>`;
