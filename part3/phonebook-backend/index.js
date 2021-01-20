70c731a13b8726ba4e3883c5b8c70a585ef062c9
Amara Bamba <amara.dianney@gmail.com>
2021-01-20 13:28:26 +0000

    Part3 ~ 3.16: Phonebook database, step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -99,6 +99,20 @@ app.get("/info", (request, response) => {
   });
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
