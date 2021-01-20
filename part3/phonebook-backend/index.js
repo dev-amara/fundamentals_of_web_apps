a9ea4dd03fe964e384617b911d06c6501194000e
Amara Bamba <amara.dianney@gmail.com>
2021-01-20 13:24:43 +0000

    Part3 ~ 3.15: Phonebook database, step3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -85,10 +85,11 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((note) => note.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => console.log(error));
 });
 
 app.get("/info", (request, response) => {
