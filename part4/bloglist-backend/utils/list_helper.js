d14038ed14c23b13820de2c6de6c36e236dcbe1e
Amara Bamba <amara.dianney@gmail.com>
2021-01-21 14:33:03 +0000

    Part4 ~ 4.5*: helper functions and unit tests, step3

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -5,7 +5,19 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) =>  blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => {return sum + blog.likes}, 0)
 
+const favoriteBlog = (blogs) => {
+  const reducer = (fav, blog) => {
+    if (blog.likes > fav.likes) {
+      return { title: blog.title, author: blog.author, likes: blog.likes }
+    }
+    return fav
+  }
+
+  return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0])
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
