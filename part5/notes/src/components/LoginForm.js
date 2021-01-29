08f5802dc23f978c4f06a9d4979bb236e7ca0b54
Amara Bamba <amara.dianney@gmail.com>
2021-01-29 10:49:03 +0000

    Part5 ~ 5.d End to end -testing

--- part5/notes/src/components/LoginForm.js
+++ part5/notes/src/components/LoginForm.js
@@ -9,13 +9,13 @@ const LoginForm = ({ handleSubmit, handleUsernameChange, handlePasswordChange, u
       <form onSubmit={handleSubmit}>
         <div>
                     username
-          <input value={username} onChange={handleUsernameChange} />
+          <input  id='username' value={username} onChange={handleUsernameChange} />
         </div>
         <div>
                     password
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input id='password' type="password" value={password} onChange={handlePasswordChange} />
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">login</button>
       </form>
     </div>
   )
--- part5/notes/src/components/Note.js
+++ part5/notes/src/components/Note.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
 const Note = ({ note, toggleImportance }) => {
-  const label = note.important
-    ? 'make not important'
-    : 'make important'
+  const label = note.important ? 'make not important' : 'make important'
 
   return (
     <li className='note'>
-      {note.content}
+      <span>{note.content}</span>
       <button onClick={toggleImportance}>{label}</button>
     </li>
   )
 }
+
 export default Note
