098f99e57b6c71604fe0e32b2de9291b6347880b
Amara Bamba <amara.dianney@gmail.com>
2021-02-07 09:51:53 +0000

    Part6 ~ 6.5*: anecdotes, step3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -6,6 +6,8 @@ const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
+  const anecdotesByVotesDesc = [...anecdotes].sort((a, b) => b.votes - a.votes);
+
   const vote = (id) => {
     console.log('vote :', id)
     dispatch(voteAnecdote(id))
@@ -21,7 +23,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotesByVotesDesc.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
