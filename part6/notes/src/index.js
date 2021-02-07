19d78aa98c974c13871aad966185e34fb68cb96b
Amara Bamba <amara.dianney@gmail.com>
2021-02-07 13:43:53 +0000

    Part6 ~ 6b

--- part6/notes/src/index.js
+++ part6/notes/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer, composeWithDevTools())
 
 ReactDOM.render(
   <Provider store={store}>
