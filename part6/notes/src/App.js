6e6b57bb8e3a871f4422817a1d001fe61a95ff0d
Amara Bamba <amara.dianney@gmail.com>
2021-02-08 11:50:13 +0000

    Part6 ~ 6c

--- part6/notes/src/App.js
+++ part6/notes/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect }  from 'react'
 import Notes from './components/Notes'
 import NewNote from './components/NewNote'
 import VisibilityFilter from './components/VisibilityFilter'
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+import noteService from './services/notes'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService
+      .getAll().then(notes => dispatch(initializeNotes(notes)))
+  }, [dispatch])
+
   return (
     <div>
       <NewNote />
--- part6/notes/src/index.js
+++ part6/notes/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { initializeNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import noteService from './services/notes'
 
 const reducer = combineReducers({
   notes: noteReducer,
@@ -13,6 +14,10 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer, composeWithDevTools())
+
+noteService.getAll().then(notes =>
+  store.dispatch(initializeNotes(notes))
+)
 
 ReactDOM.render(
   <Provider store={store}>
